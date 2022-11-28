c80666b8ae4f1d03e329f5d1e94547f054783529
bejzik8 <bejzik8@gmail.com>
2022-11-28 05:48:42 +0100

    Updated application.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import jwt from 'jsonwebtoken'
 
-import { User } from './models/User.js'
-import { GameEvent, SpecialEvent } from './models/Event.js'
-
 import authRoutes from './routes/auth.js'
 import betRoutes from './routes/bet.js'
+import adminRoutes from './routes/admin.js'
 
 dotenv.config()
 
@@ -43,74 +41,15 @@ const verifyToken = (req, res, next) => {
     }
 }
 
+const isAdmin = async (req, res, next) => {
+    if (!req.user.isAdmin) return res.send({ message: 'Not an admin!' })
+
+    next()
+}
+
 app.use('/', authRoutes)
 app.use('/user', verifyToken, betRoutes)
-
-app.post('/user', async (req, res) => {
-    const { userName } = req.body
-
-    if (!userName) return res.json({ message: 'Username is required.' })
-
-    try {
-        const data = await User.create({ userName })
-
-        res.json({ data })
-    } catch (error) {
-        return res.json({ error })
-    }
-})
-
-app.post('/game', async (req, res) => {
-    const {
-        homeTeam,
-        awayTeam,
-        date
-    } = req.body
-
-    if (!homeTeam || !awayTeam || !date) return res.json({
-        message: 'Please provide necessary event information!'
-    })
-
-    try {
-        const data = await GameEvent.create({
-            homeTeam,
-            awayTeam,
-            date,
-            availableTips: ['1', 'x', '2'],
-            award: 1
-        })
-
-        res.json({ data })
-    } catch (error) {
-        return res.json({ error })
-    }
-})
-
-app.post('/special', async (req, res) => {
-    const {
-        description,
-        date,
-        availableTips,
-        award
-    } = req.body
-
-    if (!description || !date || !availableTips || !award) return res.json({
-        message: 'Please provide necessary event information!'
-    })
-
-    try {
-        const data = await SpecialEvent.create({
-            description,
-            date,
-            availableTips,
-            award
-        })
-
-        res.json({ data })
-    } catch (error) {
-        return res.json({ error })
-    }
-})
+app.use('/admin', verifyToken, isAdmin, adminRoutes)
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`)
