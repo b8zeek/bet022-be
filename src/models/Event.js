de30b2a21d710c68216351a72d321baae9482b00
bejzik8 <bejzik8@gmail.com>
2022-11-28 03:11:34 +0100

    Created GameEvent and SpecialEvent models.

---
+++ src/models/Event.js
@@ -0,0 +1,43 @@
+import { Schema, model } from 'mongoose'
+
+const eventSchema = new Schema({
+    date: {
+        type: Date,
+        required: true
+    },
+    availableTips: {
+        type: [String],
+        required: true
+    },
+    outcome: String,
+    award: {
+        type: Number,
+        required: true
+    }
+}, { discriminatorKey: 'type' })
+
+const Event = model('Event', eventSchema)
+
+export const GameEvent = Event.discriminator(
+    'Game',
+    new Schema({
+        homeTeam: {
+            type: String,
+            required: true
+        },
+        awayTeam: {
+            type: String,
+            required: true
+        }  
+    })
+)
+
+export const SpecialEvent = Event.discriminator(
+    'Special',
+    new Schema({
+        description: {
+            type: String,
+            required: true
+        }
+    })
+)
