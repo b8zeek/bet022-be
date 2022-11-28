b9df6c428867b25f0d402150dcb0dafb69dfdd05
bejzik8 <bejzik8@gmail.com>
2022-11-28 14:04:46 +0100

    Created POST events route.

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -30,6 +30,58 @@ router.post('/game', async (req, res) => {
     }
 })
 
+router.post('/events', async (req, res) => {
+    const { events } = req.body
+
+    if (events?.length !== 0) {
+        const filteredGames = events
+            .filter(event => event?.type === 'game')
+            .filter(game => game.homeTeam && game.awayTeam && game.date)
+
+        const filteredSpecials = events
+            .filter(event => event?.type === 'special')
+            .filter(special => special.description && special.date && special.availableTips && special.award)
+
+        try {
+            if (filteredGames.length !== 0) {
+                await GameEvent.insertMany(
+                    filteredGames.map(({
+                        homeTeam,
+                        awayTeam,
+                        date
+                    }) => ({
+                        homeTeam,
+                        awayTeam,
+                        date,
+                        availableTips: ['1', 'x', '2'],
+                        award: 1
+                    }))
+                )
+            }
+
+            if (filteredSpecials.length !== 0) {
+                await SpecialEvent.insertMany(
+                    filteredSpecials.map(({
+                        description,
+                        date,
+                        availableTips,
+                        award
+                    }) => ({
+                        description,
+                        date,
+                        availableTips,
+                        award
+                    }))
+                )
+            }
+
+            res.json({ message: 'Successful!' })
+        } catch (error) {
+            res.json({ error })
+        }
+    }
+})
+
 router.post('/special', async (req, res) => {
     const {
         description,
