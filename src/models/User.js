1ebb5bf21b388d23f0b35e5b75b5f3c3cd716be5
bejzik8 <bejzik8@gmail.com>
2022-11-28 04:14:10 +0100

    Updated User model.

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,15 @@ import { Schema, model } from 'mongoose'
 const userScheme = new Schema({
     userName: {
         type: String,
-        required: true
+        required: true,
+        min: 6,
+        max: 255
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
     }
 })
 
