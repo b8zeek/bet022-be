7014bef8fa938f83e123c8a07f08cf29207a208a
Mirko Basic <bejzik8@gmail.com>
2025-04-22 03:55:17 +0200

    Finished standings route.

--- src/routes/user.js
+++ src/routes/user.js
@@ -47,8 +47,34 @@ router.post('/bets', async (req, res) => {
 router.get('/standings', async (_, res) => {
     try {
         const users = await User.find()
+        const events = await Event.find()
+        const bets = await Bet.find()
 
-        res.json({ users })
+        const finishedEvents = events.filter(event => event.outcome)
+
+        const eventOutcomes = finishedEvents.reduce((acc, event) => {
+            acc[event._id.toString()] = event.outcome
+            return acc
+        }, {})
+
+        const standings = users.map(user => {
+            const userBets = bets.filter(
+                bet => bet.userId.toString() === user._id.toString()
+            )
+
+            const correctBets = userBets.filter(
+                bet => eventOutcomes[bet.eventId.toString()] === bet.outcome
+            ).length
+
+            return {
+                ...user.toObject(),
+                correctBets
+            }
+        })
+
+        standings.sort((a, b) => b.correctBets - a.correctBets)
+
+        res.json({ standings })
     } catch (error) {
         return res.json({ error })
     }
